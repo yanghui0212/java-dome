73d26dc095ca6059e37d72cc5ead0ba927075a95
yangqianghui <yangqianghui@hotmail.com>
2019-09-07 12:07:14 +0800

    添加spring-cloud-config组件

---
+++ spring-cloud/spring-cloud-dome/config-server/src/main/java/com/yqh/ConfigServer.java
@@ -0,0 +1,20 @@
+package com.yqh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yangq
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
+}
